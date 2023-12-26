df5b7d02f4771ba038a4b7804955ffc97e6f7b42
Vignaxi Kapdee <143875442+Vignaxi21@users.noreply.github.com>
2023-12-26 12:06:12 +0000

    BodyStatLog.java Commit

---
+++ AndroidStudioProjects/WorkoutDiary/app/src/main/java/com/example/vignaxi/workoutdiary/BodyStatLog.java
@@ -0,0 +1,8 @@
+package com.example.vignaxi.workoutdiary;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class BodyStatLog implements Serializable {
+    ArrayList<BodyStats> bodystatsList = new ArrayList<BodyStats>();
+}
