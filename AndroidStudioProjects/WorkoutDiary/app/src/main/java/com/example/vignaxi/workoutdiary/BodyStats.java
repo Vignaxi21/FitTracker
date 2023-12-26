0cf5192a6bcb8a03ecb5cfad1271b1a1208631f0
Vignaxi Kapdee <143875442+Vignaxi21@users.noreply.github.com>
2023-12-26 12:07:02 +0000

    BodyStats.java Commit

---
+++ AndroidStudioProjects/WorkoutDiary/app/src/main/java/com/example/vignaxi/workoutdiary/BodyStats.java
@@ -0,0 +1,93 @@
+package com.example.vignaxi.workoutdiary;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class BodyStats implements Serializable {
+
+    static ArrayList<Double> bodyweight = new ArrayList<Double>();
+
+    private Double bodyWeight;
+    private Double bicepsSize;
+    private Double neckSize;
+    private Double wristSize;
+    private Double chestSize;
+    private Double waistSize;
+    private Double thighSize;
+    private Double calfSize;
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setBodyWeight(String bw) {
+        bodyWeight = Double.parseDouble(bw);
+    }
+
+    public Double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBicepsSize(String bis) {
+        bicepsSize = Double.parseDouble(bis);
+    }
+
+    public Double getBicepsSize() {
+        return bicepsSize;
+    }
+
+    public void setNeckSize(String ns) {
+        neckSize = Double.parseDouble(ns);
+    }
+
+    public Double getNeckSize() {
+        return neckSize;
+    }
+
+    public void setWristSize(String wrists) {
+        wristSize = Double.parseDouble(wrists);
+    }
+
+    public Double getWristSize() {
+        return wristSize;
+    }
+
+    public void setChestSize(String cs) {
+        chestSize = Double.parseDouble(cs);
+    }
+
+    public Double getChestSize() {
+        return chestSize;
+    }
+
+    public void setWaistSize(String waists) {
+        waistSize = Double.parseDouble(waists);
+    }
+
+    public Double getWaistSize() {
+        return waistSize;
+    }
+
+    public void setThighSize(String ts) {
+        thighSize = Double.parseDouble(ts);
+    }
+
+    public Double getThighSize() {
+        return thighSize;
+    }
+
+    public void setCalfSize(String calfs) {
+        calfSize = Double.parseDouble(calfs);
+    }
+
+    public Double getCalfSize() {
+        return calfSize;
+    }
+
+
+}
