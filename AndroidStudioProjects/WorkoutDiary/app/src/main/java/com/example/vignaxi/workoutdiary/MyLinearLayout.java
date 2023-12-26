33e8272021722076a7ce54e5a0bfcff4114da77c
Vignaxi Kapdee <143875442+Vignaxi21@users.noreply.github.com>
2023-12-26 12:15:02 +0000

    MyLinearLayout.java Commit

---
+++ AndroidStudioProjects/WorkoutDiary/app/src/main/java/com/example/vignaxi/workoutdiary/MyLinearLayout.java
@@ -0,0 +1,11 @@
+package com.example.vignaxi.workoutdiary;
+
+import android.widget.LinearLayout;
+
+public class MyLinearLayout {
+        public static void setupLayout(LinearLayout linlayout) {
+            //linlayout.setBackgroundColor(Color.rgb(0,0,100));
+            linlayout.setOrientation(LinearLayout.HORIZONTAL);
+            linlayout.setPadding(10, 10, 10, 10);
+        }
+    }
