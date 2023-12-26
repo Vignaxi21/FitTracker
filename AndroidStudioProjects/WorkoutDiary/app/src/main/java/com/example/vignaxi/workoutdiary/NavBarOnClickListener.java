ed1f7d73e465b5f028721b8df7740b9db5f8844a
Vignaxi Kapdee <143875442+Vignaxi21@users.noreply.github.com>
2023-12-26 12:16:46 +0000

    NavBarOnClickListener.java Commit

---
+++ AndroidStudioProjects/WorkoutDiary/app/src/main/java/com/example/vignaxi/workoutdiary/NavBarOnClickListener.java
@@ -0,0 +1,56 @@
+package com.example.vignaxi.workoutdiary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class NavBarOnClickListener implements View.OnClickListener {
+
+    int id;
+
+    public NavBarOnClickListener(int name) {
+        id = name;
+    }
+
+    public NavBarOnClickListener() {
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("Row " + id + " clicked");
+        Context context = v.getContext();
+        if (id == 1) {
+            BottomNaviClass.id = id;
+            Intent i = new Intent(context, BottomNaviClass.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        } else if (id == 2) {
+            BottomNaviClass.id = id;
+            Intent i = new Intent(context, BottomNaviClass.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+            SaveWorkout test = new SaveWorkout();
+            test.openWorkout(0, v.getContext());
+        } else if (id == 3) {
+            BottomNaviClass.id = id;
+            SaveWorkout.checkExistingFiles(v.getContext());
+            Intent i = new Intent(context, BottomNaviClass.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        } else if (id == 4) {
+            BottomNaviClass.id = id;
+            SaveBodyWeight.checkExistingFiles(v.getContext());
+            Intent i = new Intent(context, BottomNaviClass.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        } else if (id == 5) {
+            BottomNaviClass.id = id;
+            Intent i = new Intent(context, BottomNaviClass.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+
+    }
+}
